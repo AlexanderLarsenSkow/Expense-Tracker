6f3859bd2db91c0e2a30dfc01153edaaf581b9d3
Alex <askow2015@gmail.com>
2025-02-15 16:22:11 -0700

    Added Home Page with route logic

--- expense.rb
+++ expense.rb
@@ -9,5 +9,37 @@ configure do
 end
 
 get '/' do
-  "Hello World"
+  @years = session[:years]
+
+  erb :home
+end
+
+get '/add' do
+  erb :add_years
+end
+
+def set_id
+  return 1 if session[:years].empty?
+  session[:years].map { |year_list| year_list[:id] }.max + 1
+end
+
+post '/add/year' do
+  @year = params[:year]
+  session[:years] = [] unless session[:years]
+
+  session[:years] << { id: set_id, year: @year, expense_lists: [] }
+  redirect '/'
+end
+
+def set_up_year
+  @years = session[:years]
+  @year_id = params[:year_id].to_i
+  @year = @years.find { |year| year[:id] == @year_id }
+
+end
+
+get '/:year_id' do
+  set_up_year
+
+  erb :year
 end
