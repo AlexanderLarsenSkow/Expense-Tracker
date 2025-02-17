5255f5f228ffa3c103596426cc5f4e0e8549acbd
Alex <askow2015@gmail.com>
2025-02-16 17:46:36 -0700

    Added basic validation for years with associated flash messages for success and errors

--- expense.rb
+++ expense.rb
@@ -10,18 +10,29 @@ configure do
   set :session_secret, SecureRandom.hex(32)
 end
 
+helpers do
+  def flash_message(name)
+    return unless session[name]
+    "<p class = 'flash'>#{session.delete(name)}</p>"
+  end
+end
+
 get '/' do
   @years = session[:years]
 
   erb :home
 end
 
-get '/add' do
+def set_up_add_year
   @title = 'Add Year'
-  @path = 'add/year'
+  @path = '/add/year'
   @input_name = 'year'
   @input_value = params[:year]
   @label = 'Year:'
+end
+
+get '/add' do
+  set_up_add_year
 
   erb :add
 end
@@ -36,12 +47,42 @@ def set_expense_list_id
   @expense_lists.map { |list| list.id }.max + 1 
 end
 
+def existing_year?(input_year)
+  session[:years].any? { |hash| hash[:year] == input_year }
+end
+
+def invalid_size?(input)
+  !(1..100).cover? input.size
+end
+
+def invalid?(input)
+  existing_year?(input) || invalid_size?(input)
+end
+
+def determine_year_error(input)
+  if existing_year?(input)
+    session[:error] = 'Please enter a new year.'
+
+  else
+    session[:error] = 'Please enter a value between 1 and 100 characters.'
+  end
+end
+
 post '/add/year' do
-  @year = params[:year]
+  set_up_add_year
+  p @path
+  @year = params[:year].strip
   session[:years] = [] unless session[:years]
 
-  session[:years] << { id: set_year_id, year: @year, expense_lists: [] }
-  redirect '/'
+  if invalid?(@year)
+    determine_year_error(@year)
+    erb :add
+
+  else
+    session[:success] = 'Success! You have added a new year.'
+    session[:years] << { id: set_year_id, year: @year, expense_lists: [] }
+    redirect '/'
+  end
 end
 
 def set_up_year
